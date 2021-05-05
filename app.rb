c07ba03326ab66928836410c9c8cc5135dd57e23
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-05 22:25:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
   get '/newteam' do
+
     erb :newteam
   end
 
   post '/team' do
-    @team_name = params[:name]
+    @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sg]
     @sf = params[:sf]
     @pf = params[:pf]
-    @center = params[:c]
+    @c = params[:c]
+
     erb :team
   end
 
